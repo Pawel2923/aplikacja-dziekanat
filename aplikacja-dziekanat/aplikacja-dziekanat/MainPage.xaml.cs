9c247c2bd4f0dade5e7eb178a05d0de52aa2f8c0
Barbedi <146706805+Barbedi@users.noreply.github.com>
2023-10-24 17:48:30 +0200

    bledy

--- aplikacja-dziekanat/aplikacja-dziekanat/MainPage.xaml.cs
+++ aplikacja-dziekanat/aplikacja-dziekanat/MainPage.xaml.cs
@@ -14,20 +14,20 @@ namespace aplikacja_dziekanat
         public MainPage()
         {
             InitializeComponent();
-            flayout1.listview1.ItemSelected += OnSelectedItem;
+           // flayout1.listview1.ItemSelected += OnSelectedItem;
         }
     }
 
 
-    private void OnSelectedItem(
-        object sender, SelectedItemChangedEventArgs e)
-    {
-        var item = e.SelectedItem as flayout1;
-        if (item != null)
-        {
-            NavigationPage Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetType));
-            flayout1.listview1.SelectedItem = null;
-            bool IsPresented = false;
-        }
-    }
+    //private void OnSelectedItem(
+    //    object sender, SelectedItemChangedEventArgs e)
+    //{
+    //    var item = e.SelectedItem as flayout1;
+    //    if (item != null)
+    //    {
+    //        NavigationPage Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetType));
+    //        flayout1.listview1.SelectedItem = null;
+    //        bool IsPresented = false;
+    //    }
+    //}
 }
