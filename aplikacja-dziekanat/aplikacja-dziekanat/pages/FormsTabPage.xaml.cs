80824fcd64cf3d2ca82ac750274ae97013231bfd
Barbedi <146706805+Barbedi@users.noreply.github.com>
2023-10-29 23:36:48 +0100

    menu boczne

--- aplikacja-dziekanat/aplikacja-dziekanat/pages/FormsTabPage.xaml.cs
+++ aplikacja-dziekanat/aplikacja-dziekanat/pages/FormsTabPage.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -10,11 +6,31 @@ using Xamarin.Forms.Xaml;
 namespace aplikacja_dziekanat.pages
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-public partial class FormsTabPage : TabbedPage
+public partial class FormsTabPage : MasterDetailPage
 {
     public FormsTabPage()
     {
         InitializeComponent();
     }
-}
+        private void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Detail = new NavigationPage(new pages.PlanZajec());
+
+        }
+        private void Handle_Clicked1(object sender, System.EventArgs e)
+        {
+            Detail = new NavigationPage(new pages.Ogloszenia());
+
+        }
+        private void Handle_Clicked2(object sender, System.EventArgs e)
+        {
+            Detail = new NavigationPage(new pages.Pogoda());
+
+        }
+        private void Handle_Clicked3(object sender, System.EventArgs e)
+        {
+            Detail = new NavigationPage(new pages.FormsTabPage());
+
+        }
+    }
 }
