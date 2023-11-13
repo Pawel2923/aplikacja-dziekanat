e4a01a227ad3093ec6344eb271a01e4689a76cd9
Barbedi <146706805+Barbedi@users.noreply.github.com>
2023-11-13 01:20:59 +0100

    lekka zmiana stylow tekstu w planie i dodanie przyciskow do zmiany daty

--- aplikacja-dziekanat/aplikacja-dziekanat/pages/PlanZajec.xaml.cs
+++ aplikacja-dziekanat/aplikacja-dziekanat/pages/PlanZajec.xaml.cs
@@ -12,21 +12,59 @@ namespace aplikacja_dziekanat.pages
     {
         private readonly IFirebaseAuth auth = DependencyService.Get<IFirebaseAuth>();
         private DbConnection connection;
+        private DateTime currentDate;
 
         public PlanZajec()
         {
             InitializeComponent();
             InitializeListView();
+            currentDate = DateTime.Now;
+            UpdateCurrentDate();
             Device.StartTimer(TimeSpan.FromSeconds(1), () =>
             {
                 UpdateCurrentDate();
-                return true; 
+                return true;
             });
+
+            // Dodaj przyciski do przewijania dni
+            var previousDayButton = new Button { Text = "Poprzedni dzień" };
+            previousDayButton.Clicked += (sender, e) => ScrollToPreviousDay();
+
+            var nextDayButton = new Button { Text = "Następny dzień" };
+            nextDayButton.Clicked += (sender, e) => ScrollToNextDay();
+
+            var buttonsStackLayout = new StackLayout
+            {
+                Orientation = StackOrientation.Horizontal,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                Children = { previousDayButton, nextDayButton }
+            };
+
+            var mainStackLayout = new StackLayout
+            {
+                Children = { aktualnaData, lessonListView, buttonsStackLayout }
+            };
+
+            Content = mainStackLayout;
         }
+
         private void UpdateCurrentDate()
         {
+            aktualnaData.Text = currentDate.ToString("dddd, dd.MM.yyyy");
+        }
 
-            aktualnaData.Text = $"Data: {DateTime.Now.ToString("dddd, dd.MM.yyyy HH:mm:ss")}";
+        private void ScrollToNextDay()
+        {
+            currentDate = currentDate.AddDays(1);
+            UpdateCurrentDate();
+            // Tutaj możesz dodać kod do odświeżenia listy zajęć na nowy dzień
+        }
+
+        private void ScrollToPreviousDay()
+        {
+            currentDate = currentDate.AddDays(-1);
+            UpdateCurrentDate();
+            // Tutaj możesz dodać kod do odświeżenia listy zajęć na nowy dzień
         }
 
         protected override void OnAppearing()
@@ -41,48 +79,85 @@ namespace aplikacja_dziekanat.pages
         private async void GetSchedule()
         {
             connection = new DbConnection();
-            var schedule = await connection.GetSchedule("it-s-2-1", "13112023");
+            var schedule = await connection.GetSchedule("it-s-2-1", currentDate.ToString("ddMMyyyy"));
             if (schedule != null)
             {
-                lessonListView.ItemsSource = schedule; 
+                lessonListView.ItemsSource = schedule;
             }
         }
+    
 
-        private void InitializeListView()
+
+private void InitializeListView()
         {
             lessonListView.ItemTemplate = new DataTemplate(() =>
             {
-                var classTypeLabel = new Label();
-                classTypeLabel.SetBinding(Label.TextProperty, "ClassType");
+                
+                
+                    var classTypeLabel1 = new Label
+                    {
 
-                var durationLabel = new Label();
+                        TextColor = Color.White,
+                        FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label))
+                    };
+                    classTypeLabel1.SetBinding(Label.TextProperty, "ClassType");
+
+                    var durationLabel = new Label
+                    {
+
+                        TextColor = Color.White,
+                        FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label))
+                    };
                 durationLabel.SetBinding(Label.TextProperty, "Duration");
 
-                var nameLabel = new Label();
+                    var nameLabel = new Label
+                    {
+
+                        TextColor = Color.White,
+                        FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label))
+                    };
                 nameLabel.SetBinding(Label.TextProperty, "Name");
 
-                var roomLabel = new Label();
+                    var roomLabel = new Label
+                    {
+
+                        TextColor = Color.White,
+                        FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label))
+                    };
+
                 roomLabel.SetBinding(Label.TextProperty, "Room");
 
-                var teacherLabel = new Label();
+                    var teacherLabel = new Label
+                    {
+
+                        TextColor = Color.White,
+                        FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label))
+                    };
                 teacherLabel.SetBinding(Label.TextProperty, "Teacher");
 
-                var timeStartLabel = new Label();
+                    var timeStartLabel = new Label
+                    {
+
+                        TextColor = Color.White,
+                        FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label))
+                    };
                 timeStartLabel.SetBinding(Label.TextProperty, "TimeStart");
 
+               
+
+
+                    var stackLayout = new StackLayout
+                    {
+
+
+
+                        Children = { classTypeLabel1, durationLabel, nameLabel, timeStartLabel, roomLabel, teacherLabel }
+                    };
+
+                    var scrollView = new ScrollView { Content = stackLayout };
+
+                    return new ViewCell { View = stackLayout };
                 
-
-                var stackLayout = new StackLayout
-                {
-
-
-
-                    Children = { classTypeLabel, durationLabel, nameLabel, timeStartLabel, roomLabel, teacherLabel }
-                };
-
-                var scrollView = new ScrollView { Content = stackLayout };
-
-                return new ViewCell { View = stackLayout };
             });
 
             lessonListView.ItemSelected += (sender, e) =>
