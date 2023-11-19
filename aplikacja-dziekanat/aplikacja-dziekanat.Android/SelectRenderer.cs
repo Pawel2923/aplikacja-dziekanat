cc2221951cd65670a2066fe39f338cbdfc6f9951
Paweł <pawelporemba123@gmail.com>
2023-11-19 16:44:26 +0100

    Dodaj SelectRenderer

---
+++ aplikacja-dziekanat/aplikacja-dziekanat.Android/SelectRenderer.cs
@@ -0,0 +1,26 @@
+﻿using Android.Content;
+using CustomRenderer;
+using CustomRenderer.Android;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Select), typeof(SelectRenderer))]
+namespace CustomRenderer.Android
+{
+    class SelectRenderer : Xamarin.Forms.Platform.Android.AppCompat.PickerRenderer
+    {
+        public SelectRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement == null)
+            {
+                Control.Background = null;
+            }
+        }
+    }
+}
