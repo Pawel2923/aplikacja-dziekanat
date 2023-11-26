3348f401cf6f109741acd813cb4bc3626e15a1e3
Barbedi <146706805+Barbedi@users.noreply.github.com>
2023-11-26 20:08:14 +0100

    zmiana stylu w ogloszeniach

--- aplikacja-dziekanat/aplikacja-dziekanat/pages/Ogloszenia.xaml.cs
+++ aplikacja-dziekanat/aplikacja-dziekanat/pages/Ogloszenia.xaml.cs
@@ -7,44 +7,66 @@ namespace aplikacja_dziekanat.pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Ogloszenia : ContentPage
     {
-        // Przykładowe ogłoszenie (możesz zastąpić własnymi danymi)
+       
         private string aktualneOgloszenie = "Ministerstwo Edukacji i Nauki przygotowało broszurę informacyjną skierowaną do studentów, dotyczącą możliwości ubiegania się na studiach o wsparcie finansowe, w ramach bezzwrotnych świadczeń, w formie stypendiów i zapomóg finansowanych z budżetu państwa oraz kredytów studenckich z dopłatami do oprocentowania.";
 
         public Ogloszenia()
         {
             InitializeComponent();
 
-            // Dodaj Label do tytułu strony
+           
             Label pageTitle = new Label
             {
                 Text = "Wsparcie finansowe dla studentów",
                 FontSize = 24,
                 FontAttributes = FontAttributes.Bold,
                 HorizontalOptions = LayoutOptions.CenterAndExpand,
-                Margin = new Thickness(0, 10, 0, 10)
+                Margin = new Thickness(0, 10, 0, 10),
+                TextDecorations = TextDecorations.Underline,
+                TextTransform = TextTransform.Uppercase,
+                HorizontalTextAlignment = TextAlignment.Center
+               
             };
-
-            // Dodaj Label do wyświetlania ogłoszenia z tytułem i datą utworzenia
+            
             Label ogloszenieLabel = new Label
             {
-                Text = $"Tytuł: {pageTitle.Text}\n\n{aktualneOgloszenie}\n\nUtworzono: {DateTime.Now.ToString("dd.MM.yyyy")}",
+                Text = $"{aktualneOgloszenie}",
                 FontSize = 18,
                 Margin = new Thickness(10),
                 HorizontalOptions = LayoutOptions.CenterAndExpand,
                 VerticalOptions = LayoutOptions.CenterAndExpand
+
+            };
+            Label dataLabel = new Label
+            {
+
+                Text = "Data utworzenia: " + DateTime.Now.ToString("dd.MM.yyyy"),
+                FontSize = 12, 
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+            Label authorLabel = new Label
+            {
+                Text = "Autor: Paweł Kozioł",
+                FontSize = 12, 
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                VerticalOptions = LayoutOptions.CenterAndExpand
             };
 
-            // Umieść Label w kontrolce Frame
+           
             Frame frame = new Frame
             {
-                Content = ogloszenieLabel,
-                HasShadow = true, // Opcjonalne: Dodaj cień do ramki
+                Content = new StackLayout
+                {
+                    Children = { pageTitle, ogloszenieLabel,dataLabel, authorLabel}
+                },
+                HasShadow = true, 
                 Padding = new Thickness(15),
                 Margin = new Thickness(20),
-                CornerRadius = 10 // Opcjonalne: Zaokrąglij narożniki ramki
+                CornerRadius = 10 
             };
 
-            // Dodaj wszystkie elementy do głównego układu strony
+           
             Content = new StackLayout
             {
                 Children = { frame }
