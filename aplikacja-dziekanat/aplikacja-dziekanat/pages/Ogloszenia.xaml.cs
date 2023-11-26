c2803a460eb38e18b8e89a63979ec0f497204439
Barbedi <146706805+Barbedi@users.noreply.github.com>
2023-11-26 15:21:03 +0100

    dodanie tytulu oraz daty

--- aplikacja-dziekanat/aplikacja-dziekanat/pages/Ogloszenia.xaml.cs
+++ aplikacja-dziekanat/aplikacja-dziekanat/pages/Ogloszenia.xaml.cs
@@ -1,39 +1,50 @@
 ﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System;
 
 namespace aplikacja_dziekanat.pages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Ogloszenia : ContentPage
     {
-       
+        // Przykładowe ogłoszenie (możesz zastąpić własnymi danymi)
         private string aktualneOgloszenie = "Ministerstwo Edukacji i Nauki przygotowało broszurę informacyjną skierowaną do studentów, dotyczącą możliwości ubiegania się na studiach o wsparcie finansowe, w ramach bezzwrotnych świadczeń, w formie stypendiów i zapomóg finansowanych z budżetu państwa oraz kredytów studenckich z dopłatami do oprocentowania.";
 
         public Ogloszenia()
         {
             InitializeComponent();
 
+            // Dodaj Label do tytułu strony
+            Label pageTitle = new Label
+            {
+                Text = "Wsparcie finansowe dla studentów",
+                FontSize = 24,
+                FontAttributes = FontAttributes.Bold,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                Margin = new Thickness(0, 10, 0, 10)
+            };
 
+            // Dodaj Label do wyświetlania ogłoszenia z tytułem i datą utworzenia
             Label ogloszenieLabel = new Label
             {
-                Text = aktualneOgloszenie,
+                Text = $"Tytuł: {pageTitle.Text}\n\n{aktualneOgloszenie}\n\nUtworzono: {DateTime.Now.ToString("dd.MM.yyyy")}",
                 FontSize = 18,
                 Margin = new Thickness(10),
                 HorizontalOptions = LayoutOptions.CenterAndExpand,
                 VerticalOptions = LayoutOptions.CenterAndExpand
             };
 
-        
+            // Umieść Label w kontrolce Frame
             Frame frame = new Frame
             {
                 Content = ogloszenieLabel,
-                HasShadow = true,
+                HasShadow = true, // Opcjonalne: Dodaj cień do ramki
                 Padding = new Thickness(15),
                 Margin = new Thickness(20),
-                CornerRadius = 10
+                CornerRadius = 10 // Opcjonalne: Zaokrąglij narożniki ramki
             };
 
-            
+            // Dodaj wszystkie elementy do głównego układu strony
             Content = new StackLayout
             {
                 Children = { frame }
