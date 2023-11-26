8bec4b83c6f967f6c219c1861867a035bf47ef01
Barbedi <146706805+Barbedi@users.noreply.github.com>
2023-11-26 14:51:20 +0100

    dodanie pola na ogloszenia z tekstem na sztywno

--- aplikacja-dziekanat/aplikacja-dziekanat/pages/Ogloszenia.xaml.cs
+++ aplikacja-dziekanat/aplikacja-dziekanat/pages/Ogloszenia.xaml.cs
@@ -6,9 +6,38 @@ namespace aplikacja_dziekanat.pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Ogloszenia : ContentPage
     {
+       
+        private string aktualneOgloszenie = "Ministerstwo Edukacji i Nauki przygotowało broszurę informacyjną skierowaną do studentów, dotyczącą możliwości ubiegania się na studiach o wsparcie finansowe, w ramach bezzwrotnych świadczeń, w formie stypendiów i zapomóg finansowanych z budżetu państwa oraz kredytów studenckich z dopłatami do oprocentowania.";
+
         public Ogloszenia()
         {
             InitializeComponent();
+
+
+            Label ogloszenieLabel = new Label
+            {
+                Text = aktualneOgloszenie,
+                FontSize = 18,
+                Margin = new Thickness(10),
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+
+        
+            Frame frame = new Frame
+            {
+                Content = ogloszenieLabel,
+                HasShadow = true,
+                Padding = new Thickness(15),
+                Margin = new Thickness(20),
+                CornerRadius = 10
+            };
+
+            
+            Content = new StackLayout
+            {
+                Children = { frame }
+            };
         }
     }
 }
