591f8f7e372ef860e13ab61b2fdcd8062f4d5429
Paweł <pawelporemba123@gmail.com>
2023-12-14 13:11:03 +0100

    Dodaj pozostałe pola i zmień rozmiar profilu

--- aplikacja-dziekanat/aplikacja-dziekanat/pages/profilUz.xaml.cs
+++ aplikacja-dziekanat/aplikacja-dziekanat/pages/profilUz.xaml.cs
@@ -15,13 +15,22 @@ namespace aplikacja_dziekanat.pages
         private string _AlbumNumber;
         private string _Email;
         private string _PhoneNumber;
+        private string _Address;
+        private string _City;
+        private string _ZipCode;
+        private string _StudyStatus;
+        private string _Groups;
 
         public string FirstName { get { return _FirstName; } set { _FirstName = value; RaisePropertyChanged(nameof(FirstName)); } }
         public string LastName { get { return _LastName; } set { _LastName = value; RaisePropertyChanged(nameof(LastName)); } }
         public string AlbumNumber { get { return _AlbumNumber; } set { _AlbumNumber = value; RaisePropertyChanged(nameof(AlbumNumber)); } }
         public string Email { get { return _Email; } set { _Email = value; RaisePropertyChanged(nameof(Email)); } }
         public string PhoneNumber { get { return _PhoneNumber; } set { _PhoneNumber = value; RaisePropertyChanged(nameof(PhoneNumber)); } }
-
+        public string Address { get { return _Address; } set { _Address = value; RaisePropertyChanged(nameof(Address)); } }
+        public string City { get { return _City; } set { _City = value; RaisePropertyChanged(nameof(City)); } }
+        public string ZipCode { get { return _ZipCode; } set { _ZipCode = value; RaisePropertyChanged(nameof(ZipCode)); } }
+        public string StudyStatus { get { return _StudyStatus; } set { _StudyStatus = value; RaisePropertyChanged(nameof(StudyStatus)); } }
+        public string Groups { get { return _Groups; } set { _Groups = value; RaisePropertyChanged(nameof(Groups)); } }
         public profilUz()
         {
             InitializeComponent();
@@ -49,6 +58,14 @@ namespace aplikacja_dziekanat.pages
                     AlbumNumber = user.Profile.AlbumNumber;
                     Email = user.Email;
                     PhoneNumber = user.Profile.PhoneNumber;
+                    Address = user.Profile.Address;
+                    City = user.Profile.City;
+                    ZipCode = user.Profile.ZipCode;
+                    StudyStatus = user.Profile.StudyStatus;
+                    foreach (var group in user.Profile.Groups)
+                    {
+                        Groups += group + ", ";
+                    }
                     isFound = true;
                     Debug.WriteLine("Znaleziono użytkownika");
                     break;
