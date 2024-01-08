6510968a38a7d44d593100736095cb6eb85f256b
Barbedi <146706805+Barbedi@users.noreply.github.com>
2024-01-08 15:39:02 +0100

    dodanie api pogoday do aplikacji

--- aplikacja-dziekanat/aplikacja-dziekanat/pages/Pogoda.xaml.cs
+++ aplikacja-dziekanat/aplikacja-dziekanat/pages/Pogoda.xaml.cs
@@ -1,4 +1,10 @@
-﻿using Xamarin.Forms;
+﻿// Pogoda.xaml.cs
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using aplikacja_dziekanat.pages.models;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace aplikacja_dziekanat.pages
@@ -9,6 +15,45 @@ namespace aplikacja_dziekanat.pages
         public Pogoda()
         {
             InitializeComponent();
+
+            // Wywołaj metodę do pobrania danych z API
+            GetWeatherData();
+        }
+
+        private async void GetWeatherData()
+        {
+            try
+            {
+                string apiKey = "05ac3731afc62ef0200bb7a844fefb39";
+                string city = "Nowy Sącz";
+
+                string apiUrl = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}";
+
+
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string content = await response.Content.ReadAsStringAsync();
+                        WeatherData weatherData = JsonConvert.DeserializeObject<WeatherData>(content);
+
+                        // Aktualizuj etykietę z danymi pogodowymi
+                        weatherLabel.Text = $"Temperatura: {weatherData.Main.Temp}°C\nOpis: {weatherData.Weather[0].Description}";
+                    }
+                    else
+                    {
+                        // Obsłuż błędy odpowiedzi
+                        weatherLabel.Text = "Błąd podczas pobierania danych pogodowych.";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Obsłuż wyjątki
+                weatherLabel.Text = $"Wystąpił błąd: {ex.Message}";
+            }
         }
     }
 }
---
+++ aplikacja-dziekanat/aplikacja-dziekanat/pages/WeatherModels.cs
@@ -0,0 +1,18 @@
+﻿namespace aplikacja_dziekanat.pages.models
+{
+    public class WeatherData
+    {
+        public MainData Main { get; set; }
+        public WeatherDescription[] Weather { get; set; }
+    }
+
+    public class MainData
+    {
+        public float Temp { get; set; }
+    }
+
+    public class WeatherDescription
+    {
+        public string Description { get; set; }
+    }
+}
