c86acf54dc86a1cbb0b275ec1e70f2f1a359d2db
Barbedi <146706805+Barbedi@users.noreply.github.com>
2024-01-08 15:42:07 +0100

    zmiania temperatury z F na C

--- aplikacja-dziekanat/aplikacja-dziekanat/pages/Pogoda.xaml.cs
+++ aplikacja-dziekanat/aplikacja-dziekanat/pages/Pogoda.xaml.cs
@@ -3,9 +3,10 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-using aplikacja_dziekanat.pages.models;
+using aplikacja_dziekanat.pages.models;  // Popraw ścieżkę do przestrzeni nazw Models
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using aplikacja_dziekanat.pages.models;
 
 namespace aplikacja_dziekanat.pages
 {
@@ -29,7 +30,6 @@ namespace aplikacja_dziekanat.pages
 
                 string apiUrl = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}";
 
-
                 using (HttpClient client = new HttpClient())
                 {
                     HttpResponseMessage response = await client.GetAsync(apiUrl);
@@ -39,8 +39,12 @@ namespace aplikacja_dziekanat.pages
                         string content = await response.Content.ReadAsStringAsync();
                         WeatherData weatherData = JsonConvert.DeserializeObject<WeatherData>(content);
 
+                        // Przekształć temperaturę z kelwinów na stopnie Celsiusza
+                        float temperaturaWKelwinach = weatherData.Main.Temp;
+                        float temperaturaWCelsjuszach = temperaturaWKelwinach - 273.15f;
+
                         // Aktualizuj etykietę z danymi pogodowymi
-                        weatherLabel.Text = $"Temperatura: {weatherData.Main.Temp}°C\nOpis: {weatherData.Weather[0].Description}";
+                        weatherLabel.Text = $"Temperatura: {temperaturaWCelsjuszach}°C\nOpis: {weatherData.Weather[0].Description}";
                     }
                     else
                     {
