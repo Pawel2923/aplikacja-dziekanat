c04067fcb41b40e1178e66be6f8ac763db99ad0d
Paweł <pawelporemba123@gmail.com>
2024-01-08 16:16:59 +0100

    Dodaj Bindingi

--- aplikacja-dziekanat/aplikacja-dziekanat/pages/Admin.xaml.cs
+++ aplikacja-dziekanat/aplikacja-dziekanat/pages/Admin.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,31 @@ using Xamarin.Forms.Xaml;
 namespace aplikacja_dziekanat.pages
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Admin : ContentPage
+    public partial class Admin : ContentPage, INotifyPropertyChanged
     {
+        private string _Group;
+        private string _Day;
+        private string _ScheduleId;
+        private string _NoticeId;
+        private string _UserId;
+
+        public string Group { get { return _Group; } set { _Group = value; RaisePropertyChanged(nameof(Group)); } }
+        public string Day { get { return _Day; } set { _Day = value; RaisePropertyChanged(nameof(Day)); } }
+        public string ScheduleId { get { return _ScheduleId; } set { _ScheduleId = value; RaisePropertyChanged(nameof(ScheduleId)); } }
+        public string NoticeId { get { return _NoticeId; } set { _NoticeId = value; RaisePropertyChanged(nameof(NoticeId)); } }
+        public string UserId { get { return _UserId; } set { _UserId = value; RaisePropertyChanged(nameof(UserId)); } }
+
         public Admin()
         {
             InitializeComponent();
+            BindingContext = this;
+        }
+
+        new public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
